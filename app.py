b81888020289ef82d0199df3d56f8124c35ff737
Eric Thomas <thom.erc1@gmail.com>
2023-11-01 21:03:43 -0400

    Added chage page

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
