728ead89812e2376e168a37a37172a207e9863cb
Samuel Zeleke <samuelzeleke24@yahoo.com>
2023-11-29 07:56:58 -0500

    <Samuel Z> I changed my inf at the top of each my source code

--- static/ssh_key_loader.js.js
+++ static/ssh_key_loader.js.js
@@ -2,8 +2,11 @@
 
 // console.log(steg.decode('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABv0lEQVQ4T2NkYGD4ysDAwMgAAf+hNBOUDaL/QsVAFBuU/QsmBtL4E2rAHyQD2J64y8A0gmnhtqUMnMb2ICbI0H/IBmBzATYDWDiN7UEuZIFaCjYD5AKQASBBkAtggOmJuwzIFrhtUBeA1IO8AXI13IBfn7cs/s/rEwtyKiwMGJ64y4B4jAyMEDHhtqUgF8As/f314GYGbntfBsavBzf/ed+W+Y+Rm+8Pp53Pf3ZDW5AEyAXMUL/CwgBkAPOHOS1/vh/e9vfvi0cMPKEZDIxvmtP+/ziyDcn1DAyM3HyQKPn6CS7OLCHHANKEDEDqGJ+4y8CdjSJLJIfxVXnE/18XjhCpHFUZq4oOOBbALgAFyu/bFxl+nj/C8PvOFawGgrzBbmjDwKqiy8DrEwuPBRQvvJtQxvBt+zKsBoBsFJ+6AzUckKPu85bFDB8nV+L1DpdnFINQQRdcDdwL388eZHhbFY2iGWQjq6oehot4EysZ+COyUb3wNEgLJdpAsjI7n4AVPY+3wohC/tx2cDjAXfCqPIIBOTaQbUF3HSgwJRcewwzEjyumMnxZNZWBRVIOI7A+zGlh+LJ6Bjj1CaTUwL0KAG58pxl1mhXcAAAAAElFTkSuQmCC'));
 
-//@Samuel Zeleke
-/*
+/*Course Name: CMSC495 7384
+    Author: @Samuel Zeleke
+    Group: A
+    Date: Nov 23'
+    Project: CMSC495 Secure Chat Server
 SSH provides secure authentication and encrypted communication between client and server, 
 it does not involve steganography directly
  used to load SSH keys in web applications.
